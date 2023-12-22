9547e1b7cd1600bb7900538033bcdba13ffc775a
rox-streuli <roxstreuli@gmail.com>
2023-12-22 12:29:25 +0000

    add colour_list.py

---
+++ colour_list.py
@@ -0,0 +1,7 @@
+colour = ['cornflower blue', 'blue', 'deep sky blue', 'cyan',
+          'medium spring green', 'dark green', 'chartreuse',
+          'yellow', 'dark goldenrod','saddle brown',  'orange',
+          'firebrick', 'red', 'salmon', 'hot pink', 'purple',
+          'magenta', 'medium orchid', 'dark violet', 'indigo',
+          'medium slate blue'
+          ]
