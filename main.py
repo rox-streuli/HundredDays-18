63a5022f60d01446970812761dfe3ebd6bbf88a9
rox-streuli <roxstreuli@gmail.com>
2023-12-22 11:19:39 +0000

    Made timy draw geometric shapes.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from turtle import Turtle, Screen
+from colour_list import colour
+from random import choice
 
 timy = Turtle()
 # hid timy from view
@@ -6,7 +8,7 @@ timy.hideturtle()
 # do not draw a line qhile moving
 timy.penup()
 # move timy to new position
-timy.goto(-200, 0)
+timy.goto(-100, 300)
 # reveal timy new position
 timy.showturtle()
 # allow timy to write again
@@ -16,7 +18,7 @@ timy.pendown()
 timy.shape("turtle")
 # set timy new colours
 timy.color(('cyan'), ('black'))
-# timy.pencolor('#00FF7F')
+timy.pencolor('#00FF7F')
 # draw a square
 # for _ in range(4):
 #     timy.forward(100)
@@ -25,14 +27,14 @@ timy.color(('cyan'), ('black'))
 # test position
 # print(timy.pos())
 
-# set control variable
-lenght = 0
-# set loop to draw dashed line
-while lenght < 400:
-    for colour in ("#00FF7F", "white"):
-        timy.forward(10)
-        timy.color(colour)
-        lenght += 10
+# # set control variable
+# lenght = 0
+# # set loop to draw dashed line
+# while lenght < 400:
+#     for colour in ("#00FF7F", "white"):
+#         timy.forward(10)
+#         timy.color(colour)
+#         lenght += 10
 
 # print(timy.pos())
 
@@ -42,6 +44,16 @@ while lenght < 400:
 #     timy.penup()
 #     timy.forward(10)
 #     timy.pendown()
+
+# make timy draw a triangle, square, pentagon, hexagon, heptagon,
+# octagon, nonagon and decagon, one in top of another
+
+for sides in range(3, 11):
+    timy.color(choice(colour))
+    for _ in range(sides):
+        angle = 360 / sides
+        timy.forward(100)
+        timy.right(angle)
 
 screen = Screen()
 screen.exitonclick()
